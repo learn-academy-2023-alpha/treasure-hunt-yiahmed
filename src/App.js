cac4d4ef2e77f0d28e1e00361a81e6d00088ce13
yiahmed <yahyahmed28@gmail.com>
2023-03-06 15:41:57 -0800

    finished user story 4

--- src/App.js
+++ src/App.js
@@ -14,13 +14,27 @@ const App = () => {
     "?",
     "?"
   ])
+  const [treasureLocation, setTreasureLocation] = useState(Math.floor(Math.random()* board.length))
+  const [bombLocation, setbombLocation] = useState(Math.floor(Math.random()* board.length))
+  
   const handleGamePlay = (index) => {
     // alert(index)
     let updatedBoard = [...board]
+    if(index === treasureLocation) {
+      updatedBoard[index] = "💎"
+    setBoard(updatedBoard)
+    }
+    else if (index === bombLocation) {
+      updatedBoard[index] = "💣"
+      setBoard(updatedBoard)
+    }
+    else {
+
+    
     updatedBoard[index] = "🌴"
     setBoard(updatedBoard)
   }
-
+  }
   return (
     <>
       <h1>Treasure Hunt Game</h1>
